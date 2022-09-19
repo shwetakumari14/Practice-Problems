c7bbd3194ed0c56e7a6518d8d4e5ee8ac6a1f2c2
Shweta Kumari <shwetakumari@Shwetas-MacBook-Pro.local>
2022-09-19 14:54:28 -0700

    Allocate books to students

--- Pythons Solutions/Allocate Books.py
+++ Pythons Solutions/Allocate Books.py
@@ -1,40 +1,37 @@
 class Solution:
     def allocateBooks(self, arr, b):
-        n, sum = len(arr), 0
-        if n < b:
-            return -1
+        pageSum, n = 0, len(arr)
+
+        for num in arr:
+            pageSum += num
         
-        for i in range(len(arr)):
-            sum += arr[i]
-        
-        start, end, ans = 0, sum, 1000000000
-        
-        for i in range(len(arr)):
-            mid = start + (end - start)//2
+        low, high, ans = max(arr), pageSum, pageSum
+
+        while low <= high:
+            mid = low + (high - low)//2
             if self.isAllocationPossible(arr, b, mid):
                 ans = min(ans, mid)
-                end = mid - 1
+                high = mid - 1
             else:
-                start = mid + 1
-
+                low = mid + 1
+        
         return ans
-    
+        
     def isAllocationPossible(self,arr, b, curMin):
-        studentReq, currSum = 1, 0
+        currSum, n, cnt = 0, len(arr), 1
 
-        for i in range(len(arr)):
-            if arr[i] > curMin:
-                return False
+        for i in range(n):
             if currSum + arr[i] > curMin:
-                studentReq += 1
+                cnt += 1
                 currSum = arr[i]
-                if studentReq > b:
-                    return False
+
+                if cnt > b:
+                    return 0
             else:
                 currSum += arr[i]
-
         
-        return True
+        return 1
+        
 
 obj = Solution()
 arr, b = [12, 34, 67, 90], 2
